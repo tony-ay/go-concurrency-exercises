5296aaae3a7611cbc62bef68d12177a6d9ae428d
tony-ay <toay789@gmail.com>
2021-04-26 15:03:12 -0500

    My solution to 0-limit-crawler

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,12 +12,16 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
+
+var rateLimit time.Duration = 1000 * time.Millisecond
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(ticker *time.Ticker, url string, depth int, wg *sync.WaitGroup) {
 	defer wg.Done()
+	<-ticker.C
 
 	if depth <= 0 {
 		return
@@ -35,7 +39,7 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	for _, u := range urls {
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		go Crawl(ticker, u, depth-1, wg)
 	}
 	return
 }
@@ -44,6 +48,6 @@ func main() {
 	var wg sync.WaitGroup
 
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+	Crawl(time.NewTicker(rateLimit), "http://golang.org/", 4, &wg)
 	wg.Wait()
 }
